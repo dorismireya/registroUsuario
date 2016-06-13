1c99fde863b682c03528237857fc4deea24aacdf
dorismireya <mir.nereidas@gmail.com>
2016-06-13 17:59:52 -0400

    registroUsuario

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=sistema',
+    'username' => 'root',
+    'password' => 'ramirex.com',
+    'charset' => 'utf8',
+];
---
+++ config/params.php
@@ -0,0 +1,6 @@
+<?php
+return[
+'adminEmail' => 'mir.nereidas@gmail.com',
+'title' => 'Aplicación Yii',
+'salt' => 'fsddsflj38343lj0',
+];
